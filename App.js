389966f396dc07d5c1d1cd248d5398006cf346ab
huzaifa_3115 <38154499+huzaifa3115@users.noreply.github.com>
2022-02-06 00:29:41 +0500

    init

--- App.js
+++ App.js
@@ -1,13 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -26,35 +17,9 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import Login from './Screens/Login/Login';
-import Signup from './Screens/SignUp/signUp';
+import Signup from './Screens/SignUp/SignUp';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
+const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -64,7 +29,7 @@ const App: () => Node = () => {
   return (
     <SafeAreaView >
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <Signup />
+      <Login />
     </SafeAreaView>
   );
 };
--- Screens/Login/Login.js
+++ Screens/Login/Login.js
@@ -2,70 +2,59 @@
 // import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import { SafeAreaView, View, Text, StyleSheet, TextInput, Image, ImageBackground, Button, Switch, TouchableOpacity } from 'react-native';
-import  Icon  from 'react-native-vector-icons';
+import Icon from 'react-native-vector-icons';
 import ButtonComponent from '../../Components/Button/Button';
 import TextFields from '../../Components/TextBox/TextFields';
 // import Icon
 
 
 const Login = (props) => {
-    return (
-        <View style={{ padding: '10%', display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
-            <View style={{}}>
-                <View style={{ display: 'flex', flexDirection: 'row-reverse', alignContent: 'center', justifyContent: 'space-around' }}>
-                    <View>
+	return (
+		<View style={{ padding: '10%', display: 'flex', justifyContent: 'center', alignContent: 'center' }}>
+			<View style={{}}>
+				<View style={{ display: 'flex', flexDirection: 'row-reverse', alignContent: 'center', justifyContent: 'space-around' }}>
+					<Text style={{ textAlign: 'center', fontSize: 30 }}>
+						LOGIN
+					</Text>
+				</View>
+				<View>
 
-                        <Switch
-                            trackColor={{ false: "#767577", true: "#81b0ff" }}
-                        // thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-                        // ios_backgroundColor="#3e3e3e"
-                        // onValueChange={toggleSwitch}
-                        // value={isEnabled}
-                        />
-                    </View>
+					<TextFields style={styles.TextBox} placeholder='Username' />
+				</View>
+				<View>
 
-                    <Text style={{ textAlign: 'center', fontSize: 30 }}>
-                        LOGIN
-                    </Text>
-                </View>
-                <View>
+					<TextFields placeholder='Password' style={styles.TextBox} />
+				</View>
+				<ButtonComponent title="Login" />
+				<View style={{ display: 'flex', flexDirection: 'row', marginTop: '7%' }}>
+					{/* <View> */}
 
-                    <TextFields style={styles.TextBox} placeholder='Username' />
-                </View>
-                <View>
-
-                    <TextFields placeholder='Password' style={styles.TextBox} />
-                </View>
-                <ButtonComponent title="Login" />
-                <View style={{ display: 'flex', flexDirection: 'row', marginTop:'7%' }}>
-                    {/* <View> */}
-
-                    <Text>
-                        Don’t have an account yet?
-                    </Text>
-                    {/* </View>
+					<Text>
+						Don’t have an account yet?
+					</Text>
+					{/* </View>
                     <View> */}
 
-                    <TouchableOpacity><Text style={{color:'#3a76f0'}}> Sign Up </Text></TouchableOpacity>
-                    {/* </View> */}
-                    {/* <Icon
+					<TouchableOpacity><Text style={{ color: '#3a76f0' }}> Sign Up </Text></TouchableOpacity>
+					{/* </View> */}
+					{/* <Icon
                         name='rocket'
                         color='grey'
                         size={20}
                     /> */}
-                </View>
+				</View>
+			</View>
+		</View>
 
-
-            </View>
-        </View>
-
-    )
+	)
 }
+
 const styles = StyleSheet.create({
-    TextBox: {
-        backgroundColor: '#f6f6f6',
-        marginTop:'8%',
-        marginBottom:'5%'
-    }
+	TextBox: {
+		backgroundColor: '#f6f6f6',
+		marginTop: '8%',
+		marginBottom: '5%'
+	}
 });
+
 export default Login;
