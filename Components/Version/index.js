3ae4af1d0232d8f936e8d9e35ddcd91a95dda772
huzaifa3115 <sharpcreation3115@gmail.com>
2023-02-04 05:33:58 +0500

    remove incorrect sha, service notification

--- Components/Version/index.js
+++ Components/Version/index.js
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 import { Button } from 'react-native-elements'
 import styles from './style';
 
-const Version = ({ customStyle = {}, text = 'v0.89' }) => {
+const Version = ({ customStyle = {}, text = 'v0.3' }) => {
 	return (
 		<View style={[styles.versionContainer, customStyle]}>
 			<Text style={styles.versionText}>{text}</Text>
